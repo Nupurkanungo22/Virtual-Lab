496cdd42079a8c5fed73bacb07e6fec8d9e47971
Nupur Kanungo <nupurkanungo22@gmail.com>
2022-04-15 15:05:48 +0530

    first commit

--- controller.py
+++ controller.py
@@ -294,5 +294,5 @@ def logout():
     password= ""
     return render_template("index.html")
 
-if __name__ == "__main__":
-    app.run(debug= True , host="0.0.0.0")
+if __name__ == '__main__':
+    app.run(debug=True)
